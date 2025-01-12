85ece42e50582592d5f90ba5982682a7b2a80d30
René van der Ark <r.van.der.ark@gmail.com>
2025-01-12 21:27:04 +0100

    1st successful drawence.

--- addons/ez_tiles_draw/ez_tiles_draw.gd
+++ addons/ez_tiles_draw/ez_tiles_draw.gd
@@ -6,7 +6,6 @@ var select_2D_viewport_button : Button
 var select_mode_button : Button
 var prev_tile_pos := Vector2i.ZERO
 
-
 func _enter_tree() -> void:
 	dock = preload("res://addons/ez_tiles_draw/ez_tiles_draw_dock.tscn").instantiate()
 	selection = EditorInterface.get_selection()
@@ -37,25 +36,44 @@ func _get_select_mode_button() -> Button:
 		return select_mode_button
 
 
+func _tile_pos_from_mouse_pos() -> Vector2i:
+	var mouse_pos := EditorInterface.get_editor_viewport_2d().get_mouse_position()
+	var cursor_pos_on_tilemaplayer := mouse_pos - dock.under_edit.global_position
+	var tile_pos := Vector2i(cursor_pos_on_tilemaplayer / Vector2(dock.under_edit.tile_set.tile_size))
+	if cursor_pos_on_tilemaplayer.x < 0:
+		tile_pos.x -= 1
+	if cursor_pos_on_tilemaplayer.y < 0:
+		tile_pos.y -= 1
+	return tile_pos
+
+
 func _input(_event) -> void:
-	if is_instance_valid(dock.under_edit) and select_2D_viewport_button.button_pressed and dock.visible and _get_select_mode_button().button_pressed:
+	if is_instance_valid(dock.under_edit) and select_2D_viewport_button.button_pressed and _get_select_mode_button().button_pressed and dock.visible:
 		var viewport_2d := EditorInterface.get_editor_viewport_2d()
 		var g_mouse_pos = (
 			EditorInterface.get_base_control().get_global_mouse_position()
-			- viewport_2d.get_parent().global_position
+					- viewport_2d.get_parent().global_position
 		)
+
 		if viewport_2d.get_visible_rect().has_point(g_mouse_pos):
+			var tile_pos := _tile_pos_from_mouse_pos()
 			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 				viewport_2d.set_input_as_handled()
+			elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+				viewport_2d.set_input_as_handled()
 
-			var mouse_pos := viewport_2d.get_mouse_position()
-			var cursor_pos_on_tilemaplayer := mouse_pos - dock.under_edit.global_position
-			var tile_pos := Vector2i(cursor_pos_on_tilemaplayer / Vector2(dock.under_edit.tile_set.tile_size))
-			if tile_pos != prev_tile_pos:
-				dock.handle_tile_pos_changed(tile_pos, Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT))
+			if not dock.viewport_has_mouse:
+				dock.handle_mouse_entered()
+			if prev_tile_pos != tile_pos:
+				dock.handle_tile_pos_changed(tile_pos,
+						Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT),
+						Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
+				)
 				prev_tile_pos = tile_pos
-		
-		#print("---")
+		else:
+			if dock.viewport_has_mouse:
+				dock.handle_mouse_out()
+
 
 func handle_selected_node():
 	var selected_node : Node = selection.get_selected_nodes().pop_back()
--- addons/ez_tiles_draw/ez_tiles_draw_dock.gd
+++ addons/ez_tiles_draw/ez_tiles_draw_dock.gd
@@ -5,12 +5,35 @@ class_name EZTilesDrawDock
 var under_edit : TileMapLayer = null
 var hint_label : Label
 
+var prev_pos := Vector2i.ZERO
+var remembered_cells := {}
+var viewport_has_mouse := false
+
+const EZ_NEIGHBOUR_MAP := {
+	"....O...." : Vector2i.ZERO,
+	"....OX..." : Vector2i(0,3),
+	"....O..X." : Vector2i(1,0),
+	".X..O..X." : Vector2i(1,1),
+	".X..O...." : Vector2i(1,2),
+	"...XOX..." : Vector2i(1,3),
+	"...XO...." : Vector2i(2,3),
+	"....OX.X." : Vector2i(3,0),
+	".X..OX.X." : Vector2i(3,1),
+	".X..OX..." : Vector2i(3,2),
+	"...XOX.X." : Vector2i(4,0),
+	".X.XOX.X." : Vector2i(4,1),
+	".X.XOX..." : Vector2i(4,2),
+	"...XO..X." : Vector2i(5,0),
+	".X.XO..X." : Vector2i(5,1),
+	".X.XO...." : Vector2i(5,2)
+}
 
 func _enter_tree() -> void:
 	hint_label = find_child("HintLabel")
 
 
 func activate(node : TileMapLayer):
+	remembered_cells = {}
 	under_edit = node
 	hint_label.hide()
 	if under_edit.has_meta("_is_ez_tiles_generated"):
@@ -24,7 +47,78 @@ func deactivate():
 	hint_label.show()
 
 
-func handle_tile_pos_changed(tile_pos : Vector2i, lmb_pressed : bool):
-	if lmb_pressed and is_instance_valid(under_edit) and visible:
-		under_edit.set_cell(tile_pos, 0, Vector2.ZERO)
+func _place_back_remembered_cells() -> void:
+	for prev_pos in remembered_cells.keys():
+		if remembered_cells[prev_pos][0] > -1:
+			under_edit.set_cell(prev_pos, remembered_cells[prev_pos][0], remembered_cells[prev_pos][1])
+		else:
+			under_edit.erase_cell(prev_pos)
+	remembered_cells.clear()
 
+
+func _remember_cell(tile_pos : Vector2i) -> void:
+	if under_edit.get_cell_source_id(tile_pos) > -1:
+		remembered_cells[tile_pos] = [under_edit.get_cell_source_id(tile_pos), under_edit.get_cell_atlas_coords(tile_pos)]
+	else:
+		remembered_cells[tile_pos] = [-1, Vector2i.ZERO]
+
+
+func _place_cells_preview(cells_in_current_draw_area : Array[Vector2i], source_id : int) -> void:
+	for tile_pos in cells_in_current_draw_area:
+		_remember_cell(tile_pos)
+		under_edit.set_cell(tile_pos, source_id, _get_ez_atlas_coord(tile_pos))
+		_update_atlas_coords(_get_neighbors(tile_pos))
+
+
+func _commit_cell_placement(cells_in_current_draw_area : Array[Vector2i]) -> void:
+	remembered_cells.clear()
+	for tile_pos in cells_in_current_draw_area:
+		_remember_cell(tile_pos)
+		for neighbor_pos in _get_neighbors(tile_pos):
+			_remember_cell(tile_pos)
+	
+
+func _update_atlas_coords(cells : Array[Vector2i]) -> void:
+	for tile_pos in cells:
+		_remember_cell(tile_pos)
+		under_edit.set_cell(tile_pos, under_edit.get_cell_source_id(tile_pos), _get_ez_atlas_coord(tile_pos))
+
+
+func _erase_cells(cells : Array[Vector2i]):
+	for tile_pos in cells:
+		under_edit.erase_cell(tile_pos)
+		_update_atlas_coords(_get_neighbors(tile_pos))
+
+
+func _get_neighbors(tile_pos : Vector2i) -> Array[Vector2i]:
+	return [tile_pos + Vector2i.LEFT, tile_pos + Vector2i.UP, tile_pos + Vector2i.DOWN, tile_pos + Vector2i.RIGHT]
+
+
+func _get_ez_atlas_coord(tile_pos : Vector2i) -> Vector2i:
+	var l = "X" if under_edit.get_cell_source_id(tile_pos + Vector2i.LEFT) > -1 else "."
+	var r = "X" if under_edit.get_cell_source_id(tile_pos + Vector2i.RIGHT) > -1 else ".";
+	var t = "X" if under_edit.get_cell_source_id(tile_pos + Vector2i.UP) > -1 else "."
+	var b = "X" if under_edit.get_cell_source_id(tile_pos + Vector2i.DOWN) > -1 else ".";
+	var fmt = ".%s.%sO%s.%s." % [t, l, r, b]
+	return EZ_NEIGHBOUR_MAP[fmt]  if fmt in EZ_NEIGHBOUR_MAP else Vector2i.ZERO
+
+
+func handle_tile_pos_changed(tile_pos : Vector2i, lmb_pressed : bool, rmb_pressed) -> void:
+	if is_instance_valid(under_edit):
+		_place_back_remembered_cells()
+		_place_cells_preview([tile_pos], 0)
+		if lmb_pressed:
+			_commit_cell_placement([tile_pos])
+		elif rmb_pressed:
+			_erase_cells([tile_pos])
+			_commit_cell_placement([tile_pos])
+
+
+func handle_mouse_entered():
+	viewport_has_mouse = true
+	remembered_cells.clear()
+
+
+func handle_mouse_out():
+	viewport_has_mouse = false
+	_place_back_remembered_cells()
